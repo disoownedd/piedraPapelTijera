684c86a80b7d7843ec3d9f396d07aa4f5f76ab9d
diisownedd <emanueldisow@gmail.com>
2023-05-30 06:37:30 -0300

    Comienza el proceso javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,5 +19,61 @@ botonesArmas.forEach(boton =>{
 });
 
 function iniciarTurno(e){
-    contenedorPuntosUsuarios.innerText = 1;
+    let eleccionPC = Math.floor(Math.random()*3);
+    let eleccionUsuario= e.currentTarget.id;
+    // console.log(eleccionUsuario)
+
+/* 
+    piedra => 0
+    papel => 1
+    tijera => 2
+*/
+
+    if (eleccionPC === 0){
+        eleccionPC = "piedra";
+    }else if(eleccionPC === 1)
+    {eleccionPC = 'papel';
+    }else if(eleccionPC === 2){
+        eleccionPC = 'tijera'
+    }
+
+/*     console.log('Usuario' + eleccionUsuario)
+    console.log("PC" + eleccionPC) */
+
+    if(
+        (eleccionUsuario === 'piedra' && eleccionPC === 'tijera') ||
+        (eleccionUsuario === 'tijera' && eleccionPC === 'papel') ||
+        (eleccionUsuario === 'papel' && eleccionPC == 'piedra')
+    ){
+        ganaUsuario()
+    }else if(
+        (eleccionPC === 'piedra' && eleccionUsuario === 'tijera') ||
+        (eleccionPC === 'tijera' && eleccionUsuario === 'papel') ||
+        (eleccionPC === 'papel' && eleccionUsuario == 'piedra')
+    ){
+        ganaPC()
+    }else{
+        empate()
+    }
+
+    mensaje.classList.remove('disabled')
+
+    // eleccionUsuario.innerText = 
+
+}
+
+function ganaUsuario(){
+    puntosUsuarios++;
+    contenedorPuntosUsuarios.innerText = puntosUsuarios;
+    ganaPunto.innerText= 'Ganaste un punto!🔥'
+}
+
+function ganaPC(){
+    puntosPC++;
+    contenedorPuntosPC.innerText = puntosPC;
+    ganaPunto.innerText = '¡La pc ganó un punto!😭'
+}
+
+function empate(){
+    ganaPunto.innerText= '¡Empate!😱'
 }
